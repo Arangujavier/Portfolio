d4dbd82505afe5daece2fd70fc934fa7d56399af
Javier Aranguren Ortiz <70473041+Arangujavier@users.noreply.github.com>
2025-04-04 17:57:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-19/464718053_1775582523191233_4384968787957720962_n.jpg?stp=dst-jpg_s150x150_tt6&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=101&_nc_oc=Q6cZ2AHZH6zvYcaC1VaSNp_86WfqaGxzo_GQl6x_Audp6jXLDYY5OfOryheFbOfYcLGcmwCvpMHCxqbaalKIRKTqlgKO&_nc_ohc=6Rt5t7kAyh4Q7kNvgFDkVxu&_nc_gid=yPH5uXKtaMCEUskPamovyg&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AYEA68k9Hxzl3hYH5sUcV-h7pRKMduoF22sqhpxdrUkNmg&oe=67DD2913&_nc_sid=8b3546" className="App-logo" alt="logo" />
         <p>
-          Ejecutando Prueba
+          Hola mundo
         </p>
         <a
           className="App-link"
